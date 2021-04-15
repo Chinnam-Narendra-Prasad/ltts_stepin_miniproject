fa483e4b897cd6f4f3125d827bbe4df7361c6df8
keshavamurthy5 <80376117+keshavamurthy5@users.noreply.github.com>
2021-04-15 08:50:02 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,112 +1,67 @@
-#include <calculator_operations.h>
-
-/* Status of the operation requested */
-#define VALID   (1)
-#define INVALID (0)
-
-/* Calculator operation requested by user*/
-unsigned int calculator_operation = 0;
-
-/* Operands on which calculation is performed */
-int calculator_operand1 = 0;
-int calculator_operand2 = 0;
-
-/* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, EXIT };
-
-/* Display the menu of operations supported */
-void calculator_menu(void);
-/* Verifies the requested operations validity */
-int valid_operation(int operation);
-
-
-/* Start of the application */
-int main(int argc, char *argv[])
+#include "header.h"
+#include<stdio.h>
+#include<stdlib.h>
+int main()
 {
-    printf("\n****Calculator****\n");
+    AccountInfo account[1000];
+    int numberOfAccounts = 0;
+    char filename[30];
+    FILE *fptr;
+    printf("Please Enter File Name: ");
+    gets(filename);
+    fptr = fopen(filename, "r");
+    if (fptr == NULL)
+    {
+        printf("New File Created.\n");
+    }
+    if (fptr != NULL)
+    {
+        while (fread(&account[numberOfAccounts], sizeof(account[numberOfAccounts]), 1, fptr) == 1)
+        {
+            numberOfAccounts++;
+        }
+    }
+    /*
+    int choice=0;
     while(1)
     {
-        calculator_menu();
-    }
+        printf("Press 1 if you want to Register Account.\n");
+        printf("Press 2 To Search For Account.\n");
+        printf("Press 3 To Sort Accounts.\n");
+        printf("Press 4 To Print All Accounts.\n");
+        printf("Press 5 To Change Account Balance(Withdraw/Deposit)\n");
+        printf("Press 6 To Quit menu.\n");
+        printf("Please Enter Your choice: ");
+        scanf("%d",&choice);
+        if(choice == 1)
+        {
+            register_account(account,numberOfAccounts);
+        }
+        if(choice == 2)
+        {
+            search_account(account,numberOfAccounts);
+        }
+        if(choice == 3)
+        {
+            sort_account(account,numberOfAccounts);
+        }       
+        if(choice == 4)
+        {
+            print_account(account,numberOfAccounts);
+        }
+        if(choice == 5)
+        {
+            change_balance(account,numberOfAccounts);
+        }   
+        if(choice == 6)
+        {
+            write_file(account,numberOfAccounts,filename);
+            printf("Turning Off.\n");
+            break;
+        }
+	}
+       */
+    menu(account, &numberOfAccounts, filename);
+    return 0;
 }
-
-void calculator_menu(void)
-{
-    printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
-    printf("\n\tEnter your choice\n");
-   
-     // __fpurge(stdin);
-    scanf("%d", &calculator_operation);
-
-    if(EXIT == calculator_operation)
-    {
-        printf("\nThank you. Exiting the Application\n");
-        exit(0);
-    }
-
-    if(INVALID != valid_operation(calculator_operation))
-    {
-        printf("\n\tEnter your Numbers with space between them\n");
-        // __fpurge(stdin);
-        scanf("%d %d", &calculator_operand1, &calculator_operand2);
-    }
-    else
-    {
-        printf("\n\t---Wrong choice---\nEnter to continue\n");
-        // __fpurge(stdin);
-        getchar();
-        return;
-        
-    }
-    switch(calculator_operation)
-    {
-        case ADD:
-            printf("\n\t%d + %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            add(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case SUBTRACT:
-            printf("\n\t%d - %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            subtract(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case MULTIPLY:
-            printf("\n\t%d * %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            multiply(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case DIVIDE:
-            printf("\n\t%d / %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            divide(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case 5:
-            exit(0);
-            break;
-        default:
-            printf("\n\t---It should never come here---\n");
-    }
-}
-
-int valid_operation(int operation)
-{
-    /* Check if the operation is a valid operation */
-    return ((ADD <= operation) && (EXIT >= operation)) ? VALID: INVALID;
-}
+© 2021 GitHub, Inc.
